8e9ce5f97603e1f32f4574697237c593f462f352
Nikhil Ashutosh <53619972+nikhilniklulu@users.noreply.github.com>
2024-01-06 14:03:02 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Navbar } from './Components/Navbar/Navbar.jsx';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import LoginSignup from './Pages/LoginSignup.jsx';
+import Bucket from './Pages/Bucket.jsx';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory Category="mens" />} />
+          <Route path='/womens' element={<ShopCategory Category="womens" />} />
+          <Route path='/kids' element={<ShopCategory Category="kids" />} />
+          <Route path="/Product" element={<Product />}>
+            <Route path=':ProductId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Bucket/>} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import './Style.css'
+import logo from '../Assests/logo.png'
+import cart_icon from '../Assests/cart_icon.png'
+import { Link } from "react-router-dom"
+
+
+
+
+export const Navbar = () => {
+    const [menu, setMenu] = useState("Shop")
+    return (
+        <div className='navbar'>
+            <div className='nav-logo'>
+                <img src={logo} alt="logo" />
+                <p>SHOPPER</p>
+            </div>
+            <ul className='nav-menu'>
+                <li onClick={() => { setMenu("shop") }}><Link style={{ textDecoration: 'none' }} to='/' >Shop</Link>{menu === "shop" ? <h /> : <></>}</li>
+                <li onClick={() => { setMenu("mens") }}><Link style={{ textDecoration: 'none' }} to='/mens' >Men</Link>{menu === "mens" ? <h /> : <></>}</li>
+                <li onClick={() => { setMenu("women") }}><Link style={{ textDecoration: 'none' }} to='/women' >Women</Link>{menu === "women" ? <h /> : <></>}</li>
+                <li onClick={() => { setMenu("kids") }}><Link style={{ textDecoration: 'none' }} to='/kids' >Kids</Link>{menu === "kids" ? <h /> : <></>}</li>
+
+            </ul>
+            <div className='nav-login-cart'>
+                <Link to='/login'><button>Login</button></Link>
+                <Link to='/cart'><img src={cart_icon} alt="cart_icon" /></Link>
+                <div className='nav-cart-count'>0</div>
+            </div>
+
+        </div>
+    )
+}
